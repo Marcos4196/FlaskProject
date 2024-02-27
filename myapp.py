5a6012f6425225d433995a574498c602feec5756
Marcos Sanchez-Cruz <marcos.sanchez-cruz@aveva.com>
2024-02-27 02:43:01 -0800

    Added template

--- myapp.py
+++ myapp.py
@@ -1,46 +1,48 @@
 #importing the flask class . An instance of this class will be
 #the WSGI application
-from flask import Flask, redirect, url_for ,request, render_template
+from flask import Flask, redirect, url_for ,request, render_template, abort
 
 #creating an instance of the FLask Class
 app = Flask(__name__)
 
 
-users = [
-    {'id': 1 , 'name': 'john'},
-    {'id': 2 , 'name': 'nick'},
-    {'id': 3 , 'name': 'ben'}
-]
 
+PRODUCTS = {
+    'iphone':
+    {
+        'name':'Iphone 5s',
+        'category':'phone',
+        'price': 699
+    },
+    'galaxy':
+    {
+        'name':'Samsung galazy 5', 
+        'category':'phones',
+        'price':649
+    },
+    'ipad-air': 
+    {
+        'name': 'iPad Air',
+        'category': 'Tablets',
+        'price': 649,
+    },
+    'ipad-mini': {
+        'name': 'iPad Mini',
+        'category': 'Tablets',
+        'price': 549
+    }  
+}
 
 @app.route('/')
-def index():
-    return 'Index Page'
-
 @app.route('/home')
-def home_page():
-    return "Welcome Home"
+def home():
+    return render_template('home.html', products=PRODUCTS)
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
-
-
-
-#the route() decorator to tell Flask] what URL should trigger out function.
-@app.route('/user/<int:id>')
-def get_user_id(id):
-    #for user in users:
-    
-    for user in users:
-        if user['id'] == id:
-            return user['name']
-     
-
-
-
-
-
+@app.route('/product/<key>')
+def product(key):
+    product=PRODUCTS.get(key)
+    if not product:
+        abort(404)
+    return render_template('product.html',product=product)
 if __name__ =='__main__':
     app.run(host='0.0.0.0' , debug=True)
